f1af825f55869c6027b664e3aca4ddc8d51921f7
KirDemorgan <drankov.alex@yandex.ru>
2024-01-18 22:44:41 +0500

    HAHAHAHAHAHAHAH GAY

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,38 @@ use std::time::Duration;
 use std::env;
 use reqwest;
 use serde_json::json;
+use eframe::{egui, epi};
+use std::sync::{Arc, Mutex};
+use tokio::runtime::Runtime;
+
+struct App {
+    start_button: Arc<Mutex<bool>>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            start_button: Arc::new(Mutex::new(true)),
+        }
+    }
+}
+
+impl epi::App for App {
+    fn name(&self) -> &str {
+        "My Egui Application"
+    }
+
+    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame<'_>) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My Egui Application");
+
+            if ui.button("Start").clicked() {
+
+                println!("Start button clicked");
+            }
+        });
+    }
+}
 
 async fn send_telegram_message(bot_token: &str, chat_id: &str, message: &str) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("https://api.telegram.org/bot{}/sendMessage", bot_token);
@@ -24,38 +56,49 @@ async fn send_telegram_message(bot_token: &str, chat_id: &str, message: &str) ->
 
     Ok(())
 }
-#[tokio::main]
-async fn main() {
-    let bot_token = "6721764725:AAG_NR5XVUuBGFWsX9sO56Jajwc2alv0lPs";
-    let chat_id = "-4153113440";
-    loop {
-        let process_names = vec!["Code.exe", "process2", "process3"];
-        let mut running_processes = HashSet::new();
 
-        let mut sys = System::new_all();
-        sys.refresh_all();
+fn main() {
+    let app = App::default();
+    let start_button = Arc::clone(&app.start_button);
+    let bot_token = "6721764725:AAG_NR5XVUuBGFWsX9sO56Jajwc2alv0lPs".to_string();
+    let chat_id = "-4153113440".to_string();
 
-        for (_pid, process) in sys.processes() {
-            running_processes.insert(process.name().to_string());
-        }
-        println!("-----------------------------------");
-        for process_name in &process_names {
-            if running_processes.contains(*process_name) {
-                println!("Process '{}' is running.", process_name);
+    thread::spawn(move || {
+        let rt = Runtime::new().unwrap();
+        loop {
+            if *start_button.lock().unwrap() {
+                let process_names = vec!["Code.exe", "brave.exe", "process3"];
+                let mut running_processes = HashSet::new();
 
-                let message = format!("Process '{}' is running on '{}'.", process_name, env::var("USERNAME").unwrap());
-                let _ = send_telegram_message(&bot_token, &chat_id, &message).await;
+                let mut sys = System::new_all();
+                sys.refresh_all();
 
-                match env::var("USERNAME") {
-                    Ok(val) => println!("Windows username: {}", val),
-                    Err(_e) => println!("Couldn't read USERNAME environment variable"),
+                for (_pid, process) in sys.processes() {
+                    running_processes.insert(process.name().to_string());
                 }
-            } else {
-                println!("Process '{}' is not running.", process_name);
+                println!("-----------------------------------");
+                for process_name in &process_names {
+                    if running_processes.contains(*process_name) {
+                        println!("Process '{}' is running.", process_name);
+
+                        let message = format!("Process '{}' is running on '{}'.", process_name, env::var("USERNAME").unwrap());
+
+
+                        match env::var("USERNAME") {
+                            Ok(val) => println!("Windows username: {}", val),
+                            Err(_e) => println!("Couldn't read USERNAME environment variable"),
+                        }
+                    } else {
+                        println!("Process '{}' is not running.", process_name);
+                    }
+                }
+                println!("-----------------------------------");
+
+                thread::sleep(Duration::from_secs(60));
             }
         }
-        println!("-----------------------------------");
+    });
 
-        thread::sleep(Duration::from_secs(60));
-    }
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(Box::new(app), native_options);
 }
